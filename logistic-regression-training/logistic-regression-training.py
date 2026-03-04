c561e6b883d4b5dc6bb00f18b0584aff2b5162d5
Dai <50523219+Daichaaa@users.noreply.github.com>
2026-03-04 16:39:21 +0100

    Added documentation

--- logistic-regression-training/logistic-regression-training.py
+++ logistic-regression-training/logistic-regression-training.py
@@ -9,6 +9,7 @@ def train_logistic_regression(X, y, lr=0.1, steps=1000):
     Train logistic regression via gradient descent.
     Return (w, b).
     """
+    #Initialize parameters
     X = np.array(X)
     n_samples, n_features  = X.shape
     
@@ -16,11 +17,19 @@ def train_logistic_regression(X, y, lr=0.1, steps=1000):
     b = 0.0
 
     for i in range(steps):
+        #Compute predictions
         z = np.dot(X,w) + b
         p = _sigmoid(z)
+
+        #Compute loss
         loss = p - y
+        
+        #Compute the gradients
         gradient_w = np.dot(X.T, loss) / n_samples
         gradient_b = np.sum(loss) / n_samples
+        
+        #Optimization on parameters w and b
         w -= lr * gradient_w
         b -= lr * gradient_b
+        
     return (w,b)
